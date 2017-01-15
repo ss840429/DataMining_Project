f9bc0211eb4814e885d9f7b32acd267c2990bb75
Wlogsky <ss840429@gmail.com>
2017-01-16 04:55:20 +0800

    Pythonic

--- preprocess.py
+++ preprocess.py
@@ -20,34 +20,17 @@ dataSet[:rows].to_csv('wineQuality_Class1_Train.csv', index=False)
 dataSet[rows:].to_csv('wineQuality_Class1_Test.csv', index=False)
 
 density = dataSet['density']
-
-for idx, d in enumerate(density) :			## Change ph to class 0 or 1 ( sparse or dense )
-
-	if d < 1 :
-		density[idx] = 0
-	else :
-		density[idx] = 1
-
-
-
-ph = dataSet['pH']
-
-for idx, p in enumerate(ph) :			## Change ph to class 0 or 1 ( Spicy or Common )
-
-	if p < 3 :
-		ph[idx] = 0
-	else :
-		ph[idx] = 1
-
-
+ph      = dataSet['pH']
 alcohol = dataSet['alcohol']
 
-for idx, a in enumerate(alcohol) :			## Change alcohol to class 0 or 1 ( weak or strong )
 
-	if a < 9 :
-		alcohol[idx] = 0
-	else :
-		alcohol[idx] = 1
+
+for idx, (d, p, a) in enumerate(zip(density, ph, alcohol)) :			## Change density, ph and alcohol to class 0 or 1 
+
+	density[idx] = False if d < 0.995 else True 
+	ph[idx] 	 = False if p < 3 	  else True	
+	alcohol[idx] = False if a < 9 	  else True
+
 
 
 dataSet.to_csv('wineQuality_Class2.csv', index=False)
@@ -55,7 +38,5 @@ dataSet[:rows].to_csv('wineQuality_Class2_Train.csv', index=False)
 dataSet[rows:].to_csv('wineQuality_Class2_Test.csv', index=False)
 
 print(dataSet)
-
-
 
 
