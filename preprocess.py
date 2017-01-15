89db28cf8f4d4ed629a8860f4c922a86f071f18b
Wlogsky <ss840429@gmail.com>
2017-01-16 03:01:04 +0800

    DataSet and preprocessing program

---
+++ preprocess.py
@@ -0,0 +1,41 @@
+import pandas
+
+
+
+dataSet = pandas.read_csv('wineQuality.csv')
+dataSet.info()
+
+quality = dataSet['quality']
+
+for idx, q in enumerate(quality) :		## Change quality to class 0 or 1 ( Not good or Good )
+	
+	if q < 5 :
+		quality[idx] = 0
+	else :
+		quality[idx] = 1
+
+
+pandas.to_csv('wineQuality_Class1')
+
+
+ph = dataSet['pH']
+
+for idx, p in enumerate(ph) :			## Change ph to class 0 or 1 ( Spicy or Common )
+
+	if p < 3 :
+		ph[idx] = 0
+	else :
+		ph[idx] = 1
+
+
+alcohol = dataSet['alcohol']
+
+for idx, a in enumerate(alcohol) :			## Change alcohol to class 0 or 1 ( weak or strong )
+
+	if a < 9 :
+		alcohol[idx] = 0
+	else :
+		alcohol[idx] = 1
+
+
+pandas.to_csv('wineQuality_Class2')
