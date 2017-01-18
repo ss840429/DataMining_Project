9abb20a90051fc2b4a0c446f7907667f51fd6bfe
Wlogsky <ss840429@gmail.com>
2017-01-18 23:31:27 +0800

    flexible

--- BayesClassifier.py
+++ BayesClassifier.py
@@ -8,30 +8,31 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 class BayesClassifier:
-	def __init__(self, train_set) :
-		self.train = pandas.read_csv(train_set)
+	def __init__(self, train_filename, predict_class) :
+		self.train = pandas.read_csv(train_filename)
 		# self.train = self.train[800:]
 		self.train_rows = len(self.train.index)
+		self.predict_attr = predict_class
 		self.attribute_name = list(self.train)
-		self.attribute_name.remove('quality')
+		self.attribute_name.remove(predict_class)
 		self.pA = []
 		self.confusion_matrix = {}
 
 	def normalDistribution(self, attribute_name, testdata):
-		fmean = self.train[self.train['quality']==0][attribute_name].mean()
-		fvar  = self.train[self.train['quality']==0][attribute_name].var()
+		fmean = self.train[self.train[self.predict_attr]==0][attribute_name].mean()
+		fvar  = self.train[self.train[self.predict_attr]==0][attribute_name].var()
 
 		fp = (exp**(-(testdata.get(attribute_name)-fmean)**2/(2*fvar)))/(2*pi*fvar)**(1/2)
 		
-		pmean = self.train[self.train['quality']==1][attribute_name].mean()
-		pvar  = self.train[self.train['quality']==1][attribute_name].var()
+		pmean = self.train[self.train[self.predict_attr]==1][attribute_name].mean()
+		pvar  = self.train[self.train[self.predict_attr]==1][attribute_name].var()
 
 		tp = (exp**(-(testdata.get(attribute_name)-pmean)**2/(2*pvar)))/(2*pi*pvar)**(1/2)		
 
 		return fp, tp		
 
-	def discreteDistribution(self, attribute_name, testdata, m_estimate = None, m = 3):
-		ftotal = self.train[self.train['quality']==0]
+	def discreteDistribution(self, attribute_name, testdata, m_estimate = None, m = 5):
+		ftotal = self.train[self.train[self.predict_attr]==0]
 		fcount = ftotal[ftotal[attribute_name]==testdata.get(attribute_name)]
 
 		if m_estimate :
@@ -39,7 +40,7 @@ class BayesClassifier:
 		else :
 			fp = len(fcount.index)/len(ftotal.index)
 		
-		ttotal = self.train[self.train['quality']==1]
+		ttotal = self.train[self.train[self.predict_attr]==1]
 		tcount = ttotal[ttotal[attribute_name]==testdata.get(attribute_name)]
 
 		if m_estimate :
@@ -49,16 +50,16 @@ class BayesClassifier:
 
 		return fp, tp
 
-	def predict(self, test_set, m_estimate = None):
-		test = pandas.read_csv(test_set)
+	def predict(self, test_filename, m_estimate = None):
+		test = pandas.read_csv(test_filename)
 		test = test[:800]
 		self.confusion_matrix = { 'TP' : 0, 'TN' : 0, 'FP' : 0, 'FN' : 0 }
 		self.pA = []
 		print(self.attribute_name)
 
 		for idx, row in test.iterrows():
-			positive = len(self.train[self.train['quality']==1].index)/self.train_rows			## P(X|NO )P(NO )/P(X)
-			negative = len(self.train[self.train['quality']==0].index)/self.train_rows			## P(X|YES)P(YES)/P(X)
+			positive = len(self.train[self.train[self.predict_attr]==1].index)/self.train_rows			## P(X|NO )P(NO )/P(X)
+			negative = len(self.train[self.train[self.predict_attr]==0].index)/self.train_rows			## P(X|YES)P(YES)/P(X)
 
 			for attr in self.attribute_name:
 				if test[attr].dtype == np.float64:
@@ -70,17 +71,17 @@ class BayesClassifier:
 				negative *= fp 
 
 			if positive > negative:
-				if row.get('quality') == 1:
+				if row.get(self.predict_attr) == 1:
 					self.confusion_matrix['TP'] += 1
 				else:
 					self.confusion_matrix['FP'] += 1
 			else:
-				if row.get('quality') == 1:
+				if row.get(self.predict_attr) == 1:
 					self.confusion_matrix['FN'] += 1
 				else:
 					self.confusion_matrix['TN'] += 1
 
-			self.pA.append((negative, row.get('quality')))
+			self.pA.append((positive, row.get(self.predict_attr)))
 		
 		self.pA = sorted(self.pA, key = lambda x : x[0]) 	# Sort by positive 	
 		print(self.pA)
@@ -133,7 +134,7 @@ class BayesClassifier:
 		# pass
 
 
-bf = BayesClassifier('wineQuality_Class1_Train.csv')
+bf = BayesClassifier('wineQuality_Class1.csv', predict_class = 'quality' )
 bf.predict('wineQuality_Class1_Test.csv', m_estimate = False)
 bf.showConfusionMatrix()
 bf.plotROC()
